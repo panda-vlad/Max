b523230c1b9c4814c605c01c5037c4736da4776a
vlad <svlad1999@ukr.net>
2017-12-15 23:55:07 +0200

    Homework

--- myOwnClass.js
+++ myOwnClass.js
@@ -1,5 +1,37 @@
 'use strict';
+// не робить
 
 class EventEmitter {
-  
+  construcor ()
+  {
+    this.events = {};
+  }
+
+  on(eventName, fn) {
+    const event = this.events[eventName];
+    this.events[eventName] = event;
+    event.push(fn);
+  }
+
+  emit(eventName, date) {
+    const event = this.event[eventName];
+    if (event) {
+      event.forEach(function (fn) {
+        return(fn.calls(null,date));
+      })
+    }
+  }
+
 }
+
+class MyEmitter extends EventEmitter {
+
+};
+
+let my = new MyEmitter ();
+
+my.on('request', function (request) {
+  console.log(request)
+})
+
+my.emit('request', {from:'Google'});
